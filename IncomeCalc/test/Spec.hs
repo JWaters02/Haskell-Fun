1990aec4feecf8e8f8c916de31106e9812e18014
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 20:34:41 +0000

    setup student loan tests

--- IncomeCalc/test/Spec.hs
+++ IncomeCalc/test/Spec.hs
@@ -2,7 +2,7 @@ import Test.HUnit
 
 import IncomeTax
 import NITax
-import StudentLoan
+import StudentLoan (calculateStudentLoan)
 
 main :: IO ()
 main = do
@@ -10,4 +10,20 @@ main = do
     return ()
 
 allTests :: Test
-allTests = TestList []
+allTests = TestList [
+    niTaxTests,
+    incomeTaxTests,
+    studentLoanTests
+    ]
+
+studentLoanTests :: Test
+studentLoanTests = TestList [
+    TestCase (assertEqual "Student loan below threshold" 0 (calculateStudentLoan 17000)),
+    TestCase (assertEqual "Student loan above threshold" 243.45 (calculateStudentLoan 30000))
+    ]
+
+niTaxTests :: Test
+niTaxTests = TestList []
+
+incomeTaxTests :: Test
+incomeTaxTests = TestList []
