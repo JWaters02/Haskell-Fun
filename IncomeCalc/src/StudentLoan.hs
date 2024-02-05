3fb0e59d22bae62d2e057631065674312cd5af20
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 20:35:03 +0000

    first student loan calc impl

--- IncomeCalc/src/StudentLoan.hs
+++ IncomeCalc/src/StudentLoan.hs
@@ -1,4 +1,4 @@
 module StudentLoan (calculateStudentLoan) where
 
-calculateStudentLoan :: Int -> Int
-calculateStudentLoan x = if x > 25000 then x * 9 `div` 100 else 0
+calculateStudentLoan :: Int -> Double
+calculateStudentLoan x = if x > 27295 then fromIntegral (x - 27295) * 0.09 else 0
