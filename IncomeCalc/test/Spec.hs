34dbf17ea094d7115af001cd9c22c7516885735e
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 21:53:11 +0000

    write personal allowance and income tax tests

--- IncomeCalc/test/Spec.hs
+++ IncomeCalc/test/Spec.hs
@@ -2,7 +2,7 @@ import Test.HUnit
 
 import IncomeTax
 import NITax
-import StudentLoan (calculateStudentLoan)
+import StudentLoan
 
 main :: IO ()
 main = do
@@ -13,7 +13,8 @@ allTests :: Test
 allTests = TestList [
     niTaxTests,
     incomeTaxTests,
-    studentLoanTests
+    studentLoanTests,
+    personalAllowanceTests
     ]
 
 studentLoanTests :: Test
@@ -29,5 +30,17 @@ niTaxTests = TestList [
     TestCase (assertEqual "NI tax above threshold" 4529.82 (calculateNITax 60000))
     ]
 
+personalAllowanceTests :: Test
+personalAllowanceTests = TestList [
+    TestCase (assertEqual "Personal allowance below threshold" 12570 (calculatePersonalAllowance 50000)),
+    TestCase (assertEqual "Personal allowance above threshold" 2570 (calculatePersonalAllowance 120000)),
+    TestCase (assertEqual "Personal allowance well above threshold" 0 (calculatePersonalAllowance 130000))
+    ]
+
 incomeTaxTests :: Test
-incomeTaxTests = TestList []
+incomeTaxTests = TestList [
+    TestCase (assertEqual "Income tax below threshold" 0 (calculateIncomeTax 5000)),
+    TestCase (assertEqual "Income tax at basic rate" 3486 (calculateIncomeTax 30000)),
+    TestCase (assertEqual "Income tax at higher rate" 11432 (calculateIncomeTax 60000)),
+    TestCase (assertEqual "Income tax at additional rate" 76203 (calculateIncomeTax 200000))
+    ]
