e0fce9ee7b876adc2c76542a66423a378a867b7b
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 21:06:58 +0000

    first ni tax calc impl

--- IncomeCalc/src/NITax.hs
+++ IncomeCalc/src/NITax.hs
@@ -1,4 +1,9 @@
 module NITax (calculateNITax) where
 
-calculateNITax :: Int -> Double
-calculateNITax x = 0
+calculateNITax :: Int -> Float
+calculateNITax x = 
+    if x < 12584
+        then 0
+        else if x < 50284
+            then (fromIntegral (x - 12584) * 0.12 * 0.75) +  (fromIntegral (x - 12584) * 0.1 * 0.25)
+            else (fromIntegral (50284 - 12584) * 0.12 * 0.75) +  (fromIntegral (50284 - 12584) * 0.1 * 0.25) + (fromIntegral (x - 50284) * 0.02)
