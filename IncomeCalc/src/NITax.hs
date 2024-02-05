eb880fbdc83659d27199d57bdd25bb24a40ec509
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 21:32:01 +0000

    refactor ni tax code

--- IncomeCalc/src/NITax.hs
+++ IncomeCalc/src/NITax.hs
@@ -1,9 +1,31 @@
 module NITax (calculateNITax) where
 
+lowerThreshold :: Int
+lowerThreshold = 12584
+
+lowerThresholdRateQ1_3 :: Float
+lowerThresholdRateQ1_3 = 0.12
+
+lowerThresholdRateQ4 :: Float
+lowerThresholdRateQ4 = 0.1
+
+upperThreshold :: Int
+upperThreshold = 50284
+
+upperThresholdRate :: Float
+upperThresholdRate = 0.02
+
+lowerThresholdDifference :: Int -> Float
+lowerThresholdDifference income = fromIntegral (income - lowerThreshold)
+
+middleThresholdDifference :: Float
+middleThresholdDifference = fromIntegral (upperThreshold - lowerThreshold)
+
+upperThresholdDifference :: Int -> Float
+upperThresholdDifference income = fromIntegral (income - upperThreshold)
+
 calculateNITax :: Int -> Float
-calculateNITax x = 
-    if x < 12584
-        then 0
-        else if x < 50284
-            then (fromIntegral (x - 12584) * 0.12 * 0.75) +  (fromIntegral (x - 12584) * 0.1 * 0.25)
-            else (fromIntegral (50284 - 12584) * 0.12 * 0.75) +  (fromIntegral (50284 - 12584) * 0.1 * 0.25) + (fromIntegral (x - 50284) * 0.02)
+calculateNITax income
+    | income < lowerThreshold = 0
+    | income < upperThreshold = (lowerThresholdDifference income * lowerThresholdRateQ1_3 * 0.75) + (lowerThresholdDifference income * lowerThresholdRateQ4 * 0.25)
+    | otherwise = (middleThresholdDifference * lowerThresholdRateQ1_3 * 0.75) + (middleThresholdDifference * lowerThresholdRateQ4 * 0.25) + (upperThresholdDifference income * upperThresholdRate)
