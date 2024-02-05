ef79fceb03c9b443d391ac469e2fdd4b26cf5bd0
JWaters02 <watersjoshua2002@gmail.com>
2024-02-05 22:01:14 +0000

    taxable income and personal allowance impl

--- IncomeCalc/src/IncomeTax.hs
+++ IncomeCalc/src/IncomeTax.hs
@@ -1,4 +1,14 @@
-module IncomeTax (calculateIncomeTax) where
+module IncomeTax (calculateIncomeTax, calculatePersonalAllowance, calculateTaxableIncome) where
+
+calculatePersonalAllowance :: Int -> Float
+calculatePersonalAllowance x = 
+    if x > 100000
+        then max 0 (12570 - fromIntegral (x - 100000) * 0.5)
+        else 12570
+
+calculateTaxableIncome :: Int -> Int
+calculateTaxableIncome x = 
+    max 0 (x - round (calculatePersonalAllowance x))
 
 calculateIncomeTax :: Int -> Int
 calculateIncomeTax x = 0
