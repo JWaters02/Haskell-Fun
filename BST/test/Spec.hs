218825a079befc6f3b277814870a54cfa0e31708
JWaters02 <watersjoshua2002@gmail.com>
2024-05-01 21:03:45 +0100

    refactor tests to use insertMultiple

--- BST/test/Spec.hs
+++ BST/test/Spec.hs
@@ -7,7 +7,7 @@ import Prelude hiding (lookup)
 
 import BinarySearchTree (MaybeValue(..), Key, Value)
 
-import Dictionary (Dictionary, empty, lookup, insert, delete)
+import Dictionary (Dictionary, empty, lookup, insert, insertMultiple, delete)
 
 instance Arbitrary Dictionary where
   arbitrary = do
@@ -22,8 +22,8 @@ instance Arbitrary Dictionary where
 prop_empty :: Key -> Bool
 prop_empty key = lookup key empty == NothingValue
 
-prop_multiple_emptys :: [Key] -> Bool
-prop_multiple_emptys keys = all (\key -> lookup key empty == NothingValue) keys
+prop_multiple_empty :: [Key] -> Bool
+prop_multiple_empty keys = all (\key -> lookup key empty == NothingValue) keys
 
 prop_insert :: Key -> Value -> Dictionary -> Bool
 prop_insert key value dict = lookup key (insert key value dict) == JustValue value
@@ -34,6 +34,7 @@ prop_insert_multiple keyValues = all (\(key, value) -> lookup key (insert key va
 prop_delete :: Key -> Value -> Dictionary -> Bool
 prop_delete key value dict = lookup key (delete key (insert key value dict)) == NothingValue
 
+-- doesnt construct full tree, just inserts and deletes one by one
 prop_delete_multiple :: [(Key, Value)] -> Bool
 prop_delete_multiple keyValues = all (\(key, value) -> lookup key (delete key (insert key value empty)) == NothingValue) keyValues
 
@@ -41,7 +42,7 @@ qcheck_tests :: TestTree
 qcheck_tests = testGroup "QuickCheck tests"
   [ 
     testProperty "lookup on empty dictionary" prop_empty,
-    testProperty "multiple lookups on empty dictionary" prop_multiple_emptys,
+    testProperty "multiple lookups on empty dictionary" prop_multiple_empty,
     testProperty "insert then lookup on dictionary" prop_insert,
     testProperty "insert multiple then lookup on dictionary" prop_insert_multiple,
     testProperty "insert then delete then lookup on dictionary" prop_delete
@@ -52,37 +53,25 @@ qcheck_tests = testGroup "QuickCheck tests"
 
 -- unit tests
 
-test_empty :: Assertion
-test_empty = assertEqual "" NothingValue (lookup 1 empty)
-
-test_insert :: Assertion
-test_insert = assertEqual "" (JustValue "foo") (lookup 1 (insert 1 "foo" empty))
+test_lookup_wrong :: Assertion
+test_lookup_wrong = assertEqual "" NothingValue (lookup 2 (insertMultiple [(1, "foo")] empty))
 
 test_insert_multiple_same_key :: Assertion
-test_insert_multiple_same_key = assertEqual "" (JustValue "foo") (lookup 1 (insert 1 "foo" (insert 1 "bar" empty)))
+test_insert_multiple_same_key = assertEqual "" (JustValue "foo") (lookup 1 (insertMultiple [(1, "foo"), (1, "bar")] empty))
 
 test_insert_multiple_unique_key :: Assertion
-test_insert_multiple_unique_key = assertEqual "" (JustValue "bar") (lookup 2 (insert 1 "foo" (insert 2 "bar" empty)))
-
-test_delete :: Assertion
-test_delete = assertEqual "" NothingValue (lookup 1 (delete 1 (insert 1 "foo" empty)))
+test_insert_multiple_unique_key = assertEqual "" (JustValue "bar") (lookup 2 (insertMultiple [(1, "foo"), (2, "bar")] empty))
 
 test_delete_wrong :: Assertion
-test_delete_wrong = assertEqual "" (JustValue "foo") (lookup 1 (delete 2 (insert 1 "foo" empty)))
-
-test_delete_multiple :: Assertion
-test_delete_multiple = assertEqual "" (JustValue "bar") (lookup 2 (delete 1 (insert 1 "foo" (insert 2 "bar" empty))))
+test_delete_wrong = assertEqual "" (JustValue "foo") (lookup 1 (delete 2 (insertMultiple [(1, "foo")] empty)))
 
 hunit_tests :: TestTree
 hunit_tests = testGroup "HUnit tests"
   [ 
-    testCase "lookup on empty dictionary" test_empty,
-    testCase "insert then lookup on dictionary" test_insert,
+    testCase "insert then lookup wrong key on dictionary" test_lookup_wrong,
     testCase "insert multiple same key then lookup on dictionary" test_insert_multiple_same_key,
     testCase "insert multiple unique key then lookup on dictionary" test_insert_multiple_unique_key,
-    -- testCase "insert then delete then lookup on dictionary" test_delete,
     testCase "insert then delete wrong key then lookup on dictionary" test_delete_wrong
-    -- testCase "insert multiple then delete then lookup on dictionary" test_delete_multiple
   ]
 
 -------------------------------------------------------------
